a9bcadeecb33ba58ff1a17ab32248b1780b7d71b
Sam Bryant <samuellbryant@msn.com>
2018-05-31 21:18:04 -0400

    add JavaScript file assignment

---
+++ JavaScriptPrompt/prompt.js
@@ -0,0 +1,9 @@
+
+var firstName;
+var lastName;
+var age;
+
+firstName=prompt("What is your first name");
+lastName=prompt("What is your last name");
+age = prompt("What is your age");
+alert("Your name is "+firstName+ " " +lastName+"\n" +"Your age is "+age);
